225854b2276b1a55589c6d041ee05fccebd79049
Yoreni <yoreni@icloud.com>
2021-08-29 09:28:34 +0100

    Add files via upload

---
+++ src/main/java/com/yoreni/mineplugin/MineCommands.java
@@ -0,0 +1,456 @@
+package com.yoreni.mineplugin;
+
+import com.yoreni.mineplugin.util.MessageHandler;
+import com.yoreni.mineplugin.util.Placeholder;
+import com.yoreni.mineplugin.util.Util;
+import com.yoreni.mineplugin.util.WorldEditRegion;
+import com.yoreni.mineplugin.util.shape.Cuboid;
+import com.yoreni.mineplugin.util.shape.Cylinder;
+import com.yoreni.mineplugin.util.shape.Shape;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class MineCommands implements CommandExecutor, TabCompleter
+{
+    MinePlugin main = null;
+
+    public MineCommands(MinePlugin main)
+    {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args)
+    {
+        if(label.equalsIgnoreCase("mines"))
+        {
+            if(args.length == 0)
+            {
+                showHelpMenu(sender);
+            }
+
+            else if(args[0].equalsIgnoreCase("create"))
+            {
+                if(args.length < 2)
+                {
+                    sender.sendMessage("usage /mines create <name>");
+                    return true;
+                }
+
+                WorldEditRegion region = new WorldEditRegion((Player) sender);
+                if(!region.hasValidRegion())
+                {
+                    MinePlugin.getMessageHandler().sendMessage(sender, "must-make-WE-region");
+                    return true;
+                }
+
+                Shape shape;
+                String shapeName = args.length < 3 ? "cuboid" : args[2];
+                if(shapeName.equals("cylinder"))
+                {
+                    shape = new Cylinder(region.getPos1(), region.getPos2());
+                }
+                else
+                {
+                    if(!shapeName.equals("cuboid"))
+                    {
+                        MinePlugin.getMessageHandler().sendMessage(sender, "invalid-shape");
+                        return true;
+                    }
+                    shape = new Cuboid(region.getPos1(), region.getPos2());
+                }
+
+                if(Mine.get(args[1]) != null)
+                {
+                    MinePlugin.getMessageHandler().sendMessage(sender, "pick-another-name",
+                            new Placeholder("%mine%", args[1]));
+                    return true;
+                }
+
+                Mine mine = Mine.createMine(shape, args[1]);
+
+                MinePlugin.getMessageHandler().sendMessage(sender, "mine-created-success",
+                        new Placeholder("%mine%", args[1]));
+            }
+            else if(args[0].equalsIgnoreCase("add"))
+            {
+                if(args.length < 4)
+                {
+                    sender.sendMessage("usage /mines add <name> <block> <percent>");
+                    return true;
+                }
+
+                //getting and validating the mine
+                Mine mine = validdateMine(args[1], sender);
+                if(mine == null)
+                    return true;
+
+                if(!Util.getListOfBlocks().contains(args[2]))
+                {
+                    MinePlugin.getMessageHandler().sendMessage(sender, "invalid-block");
+                    return true;
+                }
+
+                Material block = Material.matchMaterial(args[2]);
+                double percent = Double.parseDouble(args[3]) / 100;
+                mine.getCompostion().addBlock(block, percent);
+
+                MinePlugin.getMessageHandler().sendMessage(sender, "block-add-success",
+                        new Placeholder("%mine%", mine.getName()),
+                        new Placeholder("%percentfilled%", Util.doubleToPercent(percent, 2)),
+                        new Placeholder("%block%", Util.materialToEnglish(block)),
+                        new Placeholder("%percentleft%", Util.doubleToPercent(1 - mine.getCompostion().getTotalComostion(), 2)));
+            }
+            else if(args[0].equalsIgnoreCase("remove"))
+            {
+                if (args.length < 3) {
+                    sender.sendMessage("usage /mines remove <name> <block>");
+                    return true;
+                }
+
+                //getting and validating the mine
+                Mine mine = validdateMine(args[1], sender);
+                if (mine == null)
+                    return true;
+
+                //if its star we will remove every block
+                if(args[2].equals("*"))
+                {
+                    mine.getCompostion().removeAllBlocks();
+                    MinePlugin.getMessageHandler().sendMessage(sender, "every-block-remove-success",
+                            new Placeholder("%mine%", mine.getName()));
+                    return true;
+                }
+
+                // otherwise we will try to remove the block specified
+                if (!Util.getListOfBlocks().contains(args[2]))
+                {
+                    MinePlugin.getMessageHandler().sendMessage(sender, "invalid-block");
+                    return true;
+                }
+
+                Material block = Material.matchMaterial(args[2]);
+                if(mine.getCompostion().hasBlock(block))
+                {
+                    mine.getCompostion().removeBlock(block);
+                    MinePlugin.getMessageHandler().sendMessage(sender, "block-remove-success",
+                            new Placeholder("%mine%", mine.getName()),
+                            new Placeholder("%block%", Util.materialToEnglish(block)),
+                            new Placeholder("%percentleft%", Util.doubleToPercent(1 - mine.getCompostion().getTotalComostion(), 2)));
+                }
+            }
+            else if(args[0].equalsIgnoreCase("reset"))
+            {
+                if(args.length < 2)
+                {
+                    sender.sendMessage("usage /mines reset <name>");
+                    return true;
+                }
+
+                //getting and validating the mine
+                Mine mine = validdateMine(args[1], sender);
+                if(mine == null)
+                    return true;
+
+                mine.reset();
+                MinePlugin.getMessageHandler().sendMessage(sender, "mine-reset-success",
+                        new Placeholder("%mine%", args[1]));
+            }
+            else if(args[0].equalsIgnoreCase("resize"))
+            {
+                if(args.length < 2)
+                {
+                    sender.sendMessage("usage /mines resize <name>");
+                    return true;
+                }
+
+                Mine mine = validdateMine(args[1], sender);
+                if(mine == null)
+                    return true;
+
+                //TODO add a get shape method
+                WorldEditRegion region = new WorldEditRegion((Player) sender);
+                Shape shape;
+                String shapeName = args.length < 3 ? mine.getShape().getName() : args[2];
+                if(shapeName.equals("cylinder"))
+                {
+                    shape = new Cylinder(region.getPos1(), region.getPos2());
+                }
+                else
+                {
+                    if(!shapeName.equals("cuboid"))
+                    {
+                        MinePlugin.getMessageHandler().sendMessage(sender, "invalid-shape");
+                        return true;
+                    }
+                    shape = new Cuboid(region.getPos1(), region.getPos2());
+                }
+
+                mine.setShape(shape);
+                sender.sendMessage("Mine " + args[1] + " resized.");
+            }
+            else if(args[0].equalsIgnoreCase("info"))
+            {
+                if(args.length < 2)
+                {
+                    sender.sendMessage("usage /mines info <name>");
+                    return true;
+                }
+
+                Mine mine = validdateMine(args[1], sender);
+                if(mine == null)
+                    return true;
+
+                MessageHandler messageHandler = MinePlugin.getMessageHandler();
+                ArrayList<String> infoLines = new ArrayList<String>();
+                infoLines.add(messageHandler.get("mine-info.title",
+                        new Placeholder("%mine%", mine.getName())
+                ));
+                if(mine.getResetInterval() > 0)
+                {
+                    String text = messageHandler.get("mine-info.reset-info",
+                            new Placeholder("%interval%", mine.getResetInterval() + ""),
+                            new Placeholder("%timeleft%", Util.formatTime(mine.getTimeUntillNextReset())));
+                    infoLines.add(text);
+                }
+                infoLines.add("");
+                infoLines.add(messageHandler.get("mine-info.contents"));
+                for(Material block : mine.getCompostion().getBlocks())
+                {
+                    String blockName = Util.materialToEnglish(block);
+                    String formattedPercent = Util.doubleToPercent(mine.getCompostion().getChance(block), 2);
+
+                    String text = messageHandler.get("mine-info.block-list",
+                            new Placeholder("%block%", blockName),
+                            new Placeholder("%percent%", formattedPercent));
+                    infoLines.add(text);
+                }
+
+                //print it to the player
+                for(String line : infoLines)
+                {
+                    sender.sendMessage(line);
+                }
+                return true;
+            }
+            else if(args[0].equalsIgnoreCase("list"))
+            {
+                MessageHandler messageHandler = MinePlugin.getMessageHandler();
+
+                ArrayList<TextComponent> infoLines = new ArrayList<TextComponent>();
+                TextComponent title = new TextComponent("Mines");
+                title.setColor(net.md_5.bungee.api.ChatColor.YELLOW);
+                title.setBold(true);
+                infoLines.add(title);
+                //TODO net.kyori.adventure.text.Component
+                for(Mine mine : Mine.getMines())
+                {
+                    TextComponent tc = new TextComponent("  - " + mine.getName());
+                    tc.setClickEvent(new ClickEvent(
+                            ClickEvent.Action.RUN_COMMAND, "/mines info " + mine.getName()));
+                    tc.setHoverEvent(
+                            new HoverEvent(HoverEvent.Action.SHOW_TEXT
+                            ,new ComponentBuilder(messageHandler.get("mine-list.click-for-info")).create())
+                    );
+
+                    infoLines.add(tc);
+                }
+
+                for(TextComponent component : infoLines)
+                {
+                    if(sender instanceof Player)
+                    {
+                        Player player = (Player) sender;
+                        player.sendMessage(component);
+                    }
+                    else
+                    {
+                        sender.sendMessage(component.getText());
+                    }
+                }
+            }
+            else if(args[0].equalsIgnoreCase("help"))
+            {
+                showHelpMenu(sender);
+            }
+            else if(args[0].equalsIgnoreCase("settings"))
+            {
+                if (args[2].equalsIgnoreCase("resetInterval"))
+                {
+                    if(args.length < 4)
+                    {
+                        sender.sendMessage("Usage /mines settings <name> resetInterval <minutes>");
+                        return true;
+                    }
+
+                    Mine mine = validdateMine(args[1], sender);
+                    if(mine == null)
+                        return true;
+
+                    int resetInterval = Integer.parseInt(args[3]);
+                    mine.setResetInterval(resetInterval);
+
+                    if(resetInterval > 0)
+                    {
+                        MinePlugin.getMessageHandler().sendMessage(sender, "mine-reset-interval-change-success",
+                                new Placeholder("%mine%", args[1]),
+                                new Placeholder("time", String.valueOf(resetInterval)));
+                    }
+                    else
+                    {
+                        MinePlugin.getMessageHandler().sendMessage(sender, "mine-reset-interval-disable-success",
+                                new Placeholder("%mine%", args[1]));
+                    }
+                }
+                else if (args[2].equalsIgnoreCase("teleportLocation"))
+                {
+                    if(!(sender instanceof Player))
+                    {
+                        MinePlugin.getMessageHandler().sendMessage(sender, "command-only-for-players");
+                        return true;
+                    }
+
+                    Mine mine = validdateMine(args[1], sender);
+                    if(mine == null)
+                        return true;
+
+                    Player player = (Player) sender;
+                    Location loc = player.getLocation();
+
+                    World world = loc.getWorld();
+                    int x = loc.getBlockX();
+                    int y = loc.getBlockY();
+                    int z = loc.getBlockZ();
+                    Location teleportPosition = new Location(world, x, y, z, loc.getYaw(), loc.getPitch());
+
+                    mine.setTeleportPosition(teleportPosition);
+                    MinePlugin.getMessageHandler().sendMessage(sender, "mine-reset-interval-change-success",
+                            new Placeholder("%mine%", args[1]),
+                            new Placeholder("%x%", String.valueOf(x)),
+                            new Placeholder("%y%", String.valueOf(y)),
+                            new Placeholder("%z%", String.valueOf(z)),
+                            new Placeholder("%world%", world.getName()));
+                }
+            }
+            else
+            {
+                showHelpMenu(sender);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args)
+    {
+        if(!sender.hasPermission("prisonmines.admin"))
+        {
+            return null;
+        }
+
+        List<String> showOnTabComplete = new ArrayList<String>();
+
+        if (args.length == 1)
+        {
+            String[] commands = {"create","add", "reset", "remove", "resize", "settings", "info", "list", "help"};
+            StringUtil.copyPartialMatches(args[0], Arrays.asList(commands), showOnTabComplete);
+            return showOnTabComplete;
+        }
+        else if (args.length == 2)
+        {
+            final List<String> subcommandBlacklist = Arrays.asList("create", "list", "help");
+            if(!subcommandBlacklist.contains(args[0]))
+            {
+                List<String> options = new ArrayList<String>();
+                for(Mine mine : Mine.getMines())
+                {
+                    options.add(mine.getName());
+                }
+
+                StringUtil.copyPartialMatches(args[1], options, showOnTabComplete);
+                return showOnTabComplete;
+            }
+            else
+            {
+                return new ArrayList<String>();
+            }
+        }
+        else if (args.length == 3)
+        {
+            if(args[0].equalsIgnoreCase("add"))
+            {
+                List<String> options = Util.getListOfBlocks();
+                StringUtil.copyPartialMatches(args[2], options, showOnTabComplete);
+                return showOnTabComplete;
+            }
+            else if(args[0].equalsIgnoreCase("create") || args[0].equalsIgnoreCase("resize"))
+            {
+                String[] shapes = {"cuboid", "cylinder"};
+                StringUtil.copyPartialMatches(args[2], Arrays.asList(shapes), showOnTabComplete);
+                return showOnTabComplete;
+            }
+            else if(args[0].equalsIgnoreCase("remove"))
+            {
+                Mine mine = Mine.get(args[1]);
+                if(mine != null)
+                {
+                    Set<Material> blocks = mine.getCompostion().getBlocks();
+                    List<String> options = new ArrayList<String>();
+                    for(Material block : blocks)
+                    {
+                        String materialName = block.getKey().asString().split(":")[1];
+                        options.add(materialName);
+                    }
+
+                    StringUtil.copyPartialMatches(args[2], options, showOnTabComplete);
+                    return showOnTabComplete;
+                }
+            }
+            else if(args[0].equalsIgnoreCase("settings"))
+            {
+                String[] commands = {"resetInterval", "teleportLocation"};
+                StringUtil.copyPartialMatches(args[2], Arrays.asList(commands), showOnTabComplete);
+                return showOnTabComplete;
+            }
+        }
+
+        return new ArrayList<String>();
+    }
+
+    private void showHelpMenu(CommandSender sender)
+    {
+        MinePlugin.getMessageHandler().sendMessage(sender, "help-menu");
+    }
+
+    private Mine validdateMine(String mineName, CommandSender sender)
+    {
+        Mine mine = Mine.get(mineName);
+        if(mine == null)
+        {
+            MinePlugin.getMessageHandler().sendMessage(sender, "invalid-mine",
+                    new Placeholder("%mine%", mineName));
+            return null;
+        }
+
+        return mine;
+    }
+}
---
+++ src/main/java/com/yoreni/mineplugin/util/shape/Cylinder.java
@@ -0,0 +1,273 @@
+package com.yoreni.mineplugin.util.shape;
+
+import de.leonhard.storage.Yaml;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Cylinder extends Shape
+{
+    private Location center;
+    private double xRadius;
+    private double zRadius;
+    private int height;
+
+    private int volume = 0;
+
+    public Cylinder(Location pos1, Location pos2)
+    {
+        if(!pos1.getWorld().equals(pos2.getWorld()))
+        {
+            throw new IllegalArgumentException("The 2 postions are not in the same world");
+        }
+
+        World world = pos1.getWorld();
+        double midX = (pos1.getBlockX() + pos2.getBlockX()) / 2;
+        double lowY = Math.min(pos1.getBlockY(), pos2.getBlockY());
+        double midZ = (pos1.getBlockZ() + pos2.getBlockZ()) / 2;
+        center = new Location(world, midX, lowY, midZ);
+
+        xRadius = (Math.abs(pos1.getBlockX() - pos2.getBlockX()) / 2) + 0.5;
+        zRadius = (Math.abs(pos1.getBlockZ() - pos2.getBlockZ()) / 2) + 0.5;
+        height = Math.abs(pos1.getBlockY() - pos2.getBlockY()) + 1;
+    }
+
+    public Cylinder(Location center, double xRadius, double zRadius, int height)
+    {
+        this.center = center;
+        this.xRadius = xRadius;
+        this.zRadius = zRadius;
+        this.height = height;
+    }
+
+    @Override
+    public List<Location> getBlocks()
+    {
+        // yes i basicly just stole the world edit code.
+        List<Location> blocks = new ArrayList<Location>();
+
+        final double invRadiusX = 1 / xRadius;
+        final double invRadiusZ = 1 / zRadius;
+
+        final int ceilRadiusX = (int) Math.ceil(xRadius);
+        final int ceilRadiusZ = (int) Math.ceil(zRadius);
+
+        double nextXn = 0;
+        forX: for (int x = 0; x <= ceilRadiusX; ++x)
+        {
+            final double xn = nextXn;
+            nextXn = (x + 1) * invRadiusX;
+            double nextZn = 0;
+            forZ: for (int z = 0; z <= ceilRadiusZ; ++z)
+            {
+                final double zn = nextZn;
+                nextZn = (z + 1) * invRadiusZ;
+
+                double distanceSq = (xn * xn) + (zn * zn);
+                if (distanceSq > 1)
+                {
+                    if (z == 0)
+                    {
+                        break forX;
+                    }
+                    break forZ;
+                }
+
+                for (int y = 0; y < height; ++y)
+                {
+                    blocks.add(new Location(getWorld(),
+                            center.getBlockX() + x,
+                            Math.ceil(center.getY() + y),
+                            center.getBlockZ() + z));
+
+                    blocks.add(new Location(getWorld(),
+                            center.getBlockX() - x,
+                            Math.ceil(center.getY() + y),
+                            center.getBlockZ() + z));
+
+                    blocks.add(new Location(getWorld(),
+                            center.getBlockX() + x,
+                            Math.ceil(center.getY() + y),
+                            center.getBlockZ() - z));
+
+                    blocks.add(new Location(getWorld(),
+                            center.getBlockX() - x,
+                            Math.ceil(center.getY() + y),
+                            center.getBlockZ() - z));
+                }
+            }
+        }
+
+        return blocks;
+    }
+
+    @Override
+    public World getWorld()
+    {
+        return center.getWorld();
+    }
+
+    @Override
+    public void writeToYaml(Yaml file, String path)
+    {
+        file.setPathPrefix(path);
+
+        file.set("shape", "cylinder");
+        file.set("world", getWorld().getName());
+        file.set("xRadius", xRadius);
+        file.set("zRadius", zRadius);
+        file.set("height", height);
+
+        file.set("center.x", center.getX());
+        file.set("center.y", center.getY());
+        file.set("center.z", center.getZ());
+    }
+
+    @Override
+    public String getName() {
+        return "cylinder";
+    }
+
+    @Override
+    public boolean isInside(Location loc)
+    {
+        if(!loc.getWorld().equals(getWorld()))
+        {
+            return false;
+        }
+
+        if(loc.getBlockY() >= center.getBlockY() && loc.getBlockY() <= center.getBlockY() + height)
+        {
+            final int x = Math.abs(loc.getBlockX()) - Math.abs(center.getBlockX());
+            final int z = Math.abs(loc.getBlockZ()) - Math.abs(center.getBlockZ());
+
+            final double invRadiusX = 1 / xRadius;
+            final double invRadiusZ = 1 / zRadius;
+
+            double xn = x * invRadiusX;
+            double zn = z * invRadiusZ;
+
+            double distanceSq = (xn * xn) + (zn * zn);
+
+            if(distanceSq > 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //TODO this could be opimised
+    @Override
+    public int getVolume()
+    {
+        //if it has already been calculated we will return that
+        if(volume != 0)
+        {
+            return volume;
+        }
+
+        Set<Location> blocks = new HashSet<Location>();
+
+        final double invRadiusX = 1 / xRadius;
+        final double invRadiusZ = 1 / zRadius;
+
+        final int ceilRadiusX = (int) Math.ceil(xRadius);
+        final int ceilRadiusZ = (int) Math.ceil(zRadius);
+
+        double nextXn = 0;
+        forX: for (int x = 0; x <= ceilRadiusX; ++x)
+        {
+            final double xn = nextXn;
+            nextXn = (x + 1) * invRadiusX;
+            double nextZn = 0;
+            forZ:
+            for (int z = 0; z <= ceilRadiusZ; ++z)
+            {
+                final double zn = nextZn;
+                nextZn = (z + 1) * invRadiusZ;
+
+                double distanceSq = (xn * xn) + (zn * zn);
+                if (distanceSq > 1) {
+                    if (z == 0) {
+                        break forX;
+                    }
+                    break forZ;
+                }
+
+                blocks.add(new Location(getWorld(),
+                        center.getBlockX() + x,
+                        0,
+                        center.getBlockZ() + z));
+
+                blocks.add(new Location(getWorld(),
+                        center.getBlockX() - x,
+                        0,
+                        center.getBlockZ() + z));
+
+                blocks.add(new Location(getWorld(),
+                        center.getBlockX() + x,
+                        0,
+                        center.getBlockZ() - z));
+
+                blocks.add(new Location(getWorld(),
+                        center.getBlockX() - x,
+                        0,
+                        center.getBlockZ() - z));
+            }
+        }
+        volume = blocks.size() * height;
+        return volume;
+    }
+
+    public static Shape readFromYaml(Yaml file, String path)
+    {
+        file.setPathPrefix(path);
+
+        if(!file.getString("shape").equals("cylinder"))
+        {
+            return null;
+        }
+
+        World world = Bukkit.getWorld(file.getString("world"));
+
+        Location center = new Location(world,
+                file.getDouble("center.x"), file.getDouble("center.y"),
+                file.getDouble("center.z"));
+        double xRadius = file.getDouble("xRadius");
+        double zRadius = file.getDouble("zRadius");
+        int height = file.getInt("height");
+
+
+        return new Cylinder(center,xRadius, zRadius, height);
+    }
+
+    public Location getCenter()
+    {
+        return center;
+    }
+
+    public double getxRadius()
+    {
+        return xRadius;
+    }
+
+    public double getzRadius()
+    {
+        return zRadius;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+}
